a7846ba90469f2dda1797353740219616b064ec0
André Pereira <andrelcop@gmail.com>
2024-10-17 16:59:10 +0200

    upload images on the new system

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -60,6 +61,21 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    const error = new Error('Not authenticated!');
+    error.code = 401;
+    throw error;
+  }
+  if(!req.file) {
+    return res.status(200).json({message: 'No file provided!'})
+  }
+  if(req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res.status(201).json({ message: 'File stored.', filePath: req.file.path })
+});
+
 app.use(
   '/graphql',
   graphqlHttp({
@@ -96,3 +112,8 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
+
+  const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
+  };
