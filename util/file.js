b3e430894f6233313019c1b6b17c43c7e8b72d92
André Pereira <andrelcop@gmail.com>
2024-10-21 09:35:30 +0200

    fix the path images folder

--- util/file.js
+++ util/file.js
@@ -2,7 +2,8 @@ const path = require('path');
 const fs = require('fs');
 
 const clearImage = filePath => {
-    filePath = path.join(__dirname, filePath);
-    fs.unlink(filePath, err => console.log(err));
-  };
-  exports.clearImage = clearImage;
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
+};
+
+exports.clearImage = clearImage;
