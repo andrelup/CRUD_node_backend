2d462371349ee3c69550a0ded347a9ab0e33af73
André Pereira <andrelcop@gmail.com>
2024-10-28 12:19:03 +0100

    add login unit test

---
+++ test/auth-controller.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
+
+describe('Auth Controller - login', () => {
+    it('should throw an error if accessing the database fails', (done) => {
+        sinon.stub(User, 'findOne');
+        User.findOne().throws();
+
+        const req = {
+            body: {
+                email: 'test1@test.es',
+                password: 'test1'
+            }
+        }
+
+        AuthController.login(req, {}, () => {}).then(result => {
+            expect(result).to.be.an('error');
+            expect(result).to.have.property('statusCode', 500);
+            done();
+        });
+        User.findOne.restore();
+    })
+})
