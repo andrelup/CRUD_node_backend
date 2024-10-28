e6e258482f3906e45d1a26aff9aad9202fdd5c0c
André Pereira <andrelcop@gmail.com>
2024-10-28 11:52:56 +0100

    get the same environment to work as in the course.

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const graphqlHttp  = require('express-graphql').graphqlHTTP;
 
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
-const auth = require('./middleware/auth');
-const { clearImage } = require('./util/file');
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -47,52 +44,11 @@ app.use((req, res, next) => {
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  /**
-   *   If we are using graphql, you all ways send a POST method, 
-   *    So if receive OPTIONS method (it is the request that it send before a POST request)
-   *    we just send 200 code back
-   */
-  if(req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
   next();
 });
 
-app.use(auth);
-
-app.put('/post-image', (req, res, next) => {
-  if (!req.isAuth) {
-    const error = new Error('Not authenticated!');
-    error.code = 401;
-    throw error;
-  }
-  if(!req.file) {
-    return res.status(200).json({message: 'No file provided!'})
-  }
-  if(req.body.oldPath) {
-    clearImage(req.body.oldPath);
-  }
-  return res.status(201).json({ message: 'File stored.', filePath: req.file.path })
-});
-
-app.use(
-  '/graphql',
-  graphqlHttp({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-    formatError(err) {
-      if (!err.originalError) {
-        return err;
-      }
-      const data = err.originalError.data;
-      const message = err.message || 'An error occurred.';
-      const code = err.originalError.code || 500;
-      return { message: message, status: code, data: data };
-    }
-  })
-);
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -112,4 +68,3 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
-
